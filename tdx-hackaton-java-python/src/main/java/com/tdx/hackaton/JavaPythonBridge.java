b91b3049a496d124b0b6f37ba7df7eca36d4f351
Marcos Góis <marcos.gois@talkdesk.com>
2019-11-04 15:41:51 +0000

    Fixed the multiplier.py script with the required collection conversions.

--- tdx-hackaton-java-python/src/main/java/com/tdx/hackaton/JavaPythonBridge.java
+++ tdx-hackaton-java-python/src/main/java/com/tdx/hackaton/JavaPythonBridge.java
@@ -9,7 +9,6 @@ import org.springframework.core.io.ClassPathResource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Arrays;
--- tdx-hackaton-java-python/src/main/resources/scripts/multiplier.py
+++ tdx-hackaton-java-python/src/main/resources/scripts/multiplier.py
@@ -1,1 +1,6 @@
+from py4j.java_collections import SetConverter, MapConverter, ListConverter
+
 result = list(map(lambda x: x*2, numbers))
+
+# Required conversions
+result = ListConverter().convert(result, gateway._gateway_client)
